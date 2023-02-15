a7b85d82a7c6a17158c1eb2ecfc12c43b5191afa
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-15 17:58:41 -0300

    Feat: Resolvendo Exercicio 04

--- src/main.js
+++ src/main.js
@@ -3,16 +3,29 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const containerSection = document.querySelector('.loading');
 const productList = document.querySelector('.products');
-
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const products = async () => {
+const createLoadElement = () => {
+  const createElement = document.createElement('div');
+  createElement.innerText = 'carregando...';
+  createElement.classList.add('loading');
+  containerSection.appendChild(createElement);
+  return createElement;
+};
+
+const loadElement = createLoadElement();
+
+const CreateProducts = async () => {
   const results = await fetchProductsList('computador');
   results.forEach((product) => {
     const createElement = createProductElement(product);
     productList.appendChild(createElement);
   });
+  loadElement.remove();
 };
 
-products();
+window.onload = () => {
+  CreateProducts();
+};
