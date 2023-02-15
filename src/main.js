eff630f0d5e3b36dd08d07b0a694372e7485fbc4
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-15 18:14:51 -0300

    Feat: Resolvendo Exercicio 05

--- src/main.js
+++ src/main.js
@@ -3,26 +3,37 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
-const containerSection = document.querySelector('.loading');
+const createElement = document.createElement('div');
+const headerDiv = document.querySelector('.loading');
 const productList = document.querySelector('.products');
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const createLoadElement = () => {
-  const createElement = document.createElement('div');
   createElement.innerText = 'carregando...';
   createElement.classList.add('loading');
-  containerSection.appendChild(createElement);
+  headerDiv.appendChild(createElement);
+  return createElement;
+};
+
+const createError = () => {
+  createElement.innerText = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  createElement.classList.add('error');
+  headerDiv.appendChild(createElement);
   return createElement;
 };
 
 const loadElement = createLoadElement();
 
 const CreateProducts = async () => {
-  const results = await fetchProductsList('computador');
-  results.forEach((product) => {
-    const createElement = createProductElement(product);
-    productList.appendChild(createElement);
-  });
+  try {
+    const results = await fetchProductsList('computador');
+    results.forEach((product) => {
+      const element = createProductElement(product);
+      productList.appendChild(element);
+    });
+  } catch (error) {
+    createError();
+  }
   loadElement.remove();
 };
 
