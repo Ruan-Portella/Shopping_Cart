7caf247428f8626ec3de9fb8a4cec17cd3c0b05d
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-15 19:11:05 -0300

    Feat: Resolvendo Exercicio 06 e 07

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,12 +1,16 @@
-export const fetchProduct = () => {
-  // seu código aqui
+export const fetchProduct = async (param) => {
+  const URL_BASE = `https://api.mercadolibre.com/items/${param}`;
+  if (!param) throw new Error('ID não informado');
+  const response = await fetch(URL_BASE);
+  const data = await response.json();
+  return data;
 };
 
 export const fetchProductsList = async (query) => {
+  const URL_BASE = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
   if (!query) {
     throw new Error('Termo de busca não informado');
   }
-  const URL_BASE = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
   const response = await fetch(URL_BASE);
   const data = await response.json();
   return data.results;
--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,22 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('Teste se fetchProduct é uma função', async () => {
+    expect(typeof fetchProduct).toBe('function');
+  });
+  it('FetchProduct com argumento "MLB1405519561" e veja se retorna fetch', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('fetchProduct com argumento "MLB1405519561", utiliza o endpoint ', async () => {
+    await fetchProduct('MLB1405519561');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561');
+  });
+  it('fetchProduct com argumento "MLB1405519561" é uma estrutura de dados', async () => {
+    const param = await fetchProduct('MLB1405519561');
+    expect(param).toEqual(product);
+  });
+  it('fetchProduct sem argumento, retorna erro: ID não informado', async () => {
+    await expect(() => fetchProduct()).rejects.toThrow('ID não informado');
   });
 });
