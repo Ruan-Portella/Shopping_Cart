470fb3331d0bc0aae2f2361f1734ee14688032d4
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-15 16:55:10 -0300

    Feat: Resolvendo Exercicio 02

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (query) => {
+  if (!query) {
+    throw new Error('Termo de busca não informado');
+  }
+  const URL_BASE = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const response = await fetch(URL_BASE);
+  const data = await response.json();
+  return data.results;
 };
