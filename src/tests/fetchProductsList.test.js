cb942083473d0bbbc799fceac06b05b8a9f7c45c
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-15 16:43:26 -0300

    Feat: Resolvendo Exercicio 01

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
+  it('fetchProductsList com argumento é um objeto de computadorSearch', async () => {
+    const save = await fetchProductsList('computador');
+    expect(save).toEqual(computadorSearch);
+  });
+
+  it('fetchProductsList sem argumento retorna um erro', async () => {
+    await expect(() => fetchProductsList())
+      .rejects.toThrow('Termo de busca não informado');
+  });
 });
