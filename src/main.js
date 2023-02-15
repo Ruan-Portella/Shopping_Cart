50969d751c5a7f98479094bee2f84e59a9fff506
Ruan-Portella <ruanmorales29@gmail.br>
2023-02-15 17:12:47 -0300

    Feat: Resolvendo Exercicio 03

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const productList = document.querySelector('.products');
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const products = async () => {
+  const results = await fetchProductsList('computador');
+  results.forEach((product) => {
+    const createElement = createProductElement(product);
+    productList.appendChild(createElement);
+  });
+};
+
+products();
